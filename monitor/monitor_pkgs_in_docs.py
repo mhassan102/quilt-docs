0c85b08cee0ff1316f5b6afaec4f92155b74d1ce
mhassan102 <m.hassan_102@yahoo.com>
2017-12-08 23:48:37 +0000

    monitor-logs-packages: code cleaning

--- monitor/monitor_pkgs_in_docs.py
+++ monitor/monitor_pkgs_in_docs.py
@@ -13,7 +13,6 @@
 
 import os
 import re
-from termcolor import colored
 
 # Global vars
 CWD = os.getcwd()
@@ -28,7 +27,7 @@ def check_quilt_log(user_pkg):
     print("**Checking package " + user_pkg + "..")
     retcode = os.system("quilt log "+ user_pkg +" > /dev/null 2>&1")
     if retcode != 0:
-        print(colored("Package " + user_pkg + " not exist \n", 'red'))
+        print("Package " + user_pkg + " not exist \n")
 
 def main():
     """ Main handler """
@@ -36,16 +35,16 @@ def main():
     # Start parsing file in docs
     #
     for file in [file_ for file_ in os.listdir(REPO_PATH) if file_.endswith(".md")]:
-        with open(os.path.join(REPO_PATH, file)) as file_:
-            for line in file_:
+        with open(os.path.join(REPO_PATH, file)) as fh_:
+            for line in fh_:
                 # checking similar lines with 'quilt.data.user import pkg'
-                grp = re.search(r'from quilt.(data|vfs)[.]([a-zA-Z0-9_]{4,12}) import ([a-zA-Z0-9_]{4,50})', line)
+                grp = re.search(r'from\s+quilt[.](data|vfs)[.]([a-zA-Z0-9_]{4,12})\s+import\s+([a-zA-Z0-9_]{4,50})', line)
                 if grp:
                     pkg_user = "{}/{}".format(grp.group(2), grp.group(3))
                     check_quilt_log(pkg_user)
 
                 # checking similar lines with 'quilt install user\pkg'
-                grp = re.search(r'quilt install ([a-zA-Z0-9_]{4,12})\/([a-zA-Z0-9_]{4,50})', line)
+                grp = re.search(r'quilt\s+install\s+([a-zA-Z0-9_]{4,12})\/([a-zA-Z0-9_]{4,50})', line)
                 if grp:
                     pkg_user = "{}/{}".format(grp.group(1), grp.group(2))
                     check_quilt_log(pkg_user)
